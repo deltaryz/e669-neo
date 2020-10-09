d76dcf5c05c86d4d927803aa00ed0b6df89e406b
techniponi <wincam97@gmail.com>
2020-10-08 23:55:04 -0500

    implement site selector dropdown

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello gay stinky world");
+// e669-neo
+// © 2020 Cameron Seid
+
+// grab the elements from the webpage
+let searchBox = document.getElementById("searchBox");
+let selectorE621 = document.getElementById("selectorE621");
+let selectorDerpibooru = document.getElementById("selectorDerpibooru");
+
+// set the current API to e621
+selectorE621.onclick = function () {
+
+}
+
+// set the current API to Derpibooru
+selectorDerpibooru.onclick = function () {
+
+}
