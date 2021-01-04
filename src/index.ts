3a4129f1603c5ec7b5b347a4338d14bf1f8f244b
deltaryz <wincam97@gmail.com>
2021-01-04 03:12:11 -0600

    implement dropdown icon changing

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,28 @@
 // e669-neo
 // © 2020 Cameron Seid
 
+export enum API {
+    E621,
+    DERPIBOORU
+}
+
+// global variables
+export let currentApi = API.E621;
+
 // grab the elements from the webpage
 let searchBox = document.getElementById("searchBox");
 let selectorE621 = document.getElementById("selectorE621");
 let selectorDerpibooru = document.getElementById("selectorDerpibooru");
+let websiteDropdownImg = document.getElementById("websiteDropdown");
 
 // set the current API to e621
 selectorE621.onclick = function () {
-
+    currentApi = API.E621;
+    websiteDropdownImg.setAttribute("src", "assets/e621-icon.png");
 }
 
 // set the current API to Derpibooru
 selectorDerpibooru.onclick = function () {
-
+    currentApi = API.DERPIBOORU;
+    websiteDropdownImg.setAttribute("src", "assets/derpi-icon.png");
 }
