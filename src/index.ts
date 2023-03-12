f2fa3e1cc3abc5f7f9207f9027a45670a1154c94
deltaryz <wincam97@gmail.com>
2023-03-12 07:00:05 -0500

    scale grid to be mobile-friendly

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,25 @@ export enum LOCATION {
   SETTINGS,
 }
 
+// check if viewport dimensions are mobile-sized
+let isMobile = function () {
+  return window.innerWidth < 768;
+}
+
+// resize the grid of images
+let resizeGrid = function (size: string) {
+  const gridsizers = document.querySelectorAll(".grid-sizer");
+  const griditems = document.querySelectorAll(".grid-item");
+  // combine all of them together since we're doing the same thing to all of them
+  const elements = Array.from(gridsizers).concat(Array.from(griditems));
+
+  for (let i = 0; i < elements.length; i++) {
+    const element = elements[i] as HTMLElement;
+    // set 'width' to the given size
+    element.style.width = size;
+  }
+}
+
 // global variables
 export let currentApi: API;
 export let currentLocation: LOCATION;
@@ -38,6 +57,7 @@ fetch("header.html")
 
     // switch to e621 api when clicked
     selectorE621.onclick = function () {
+      // set dropdown image to e6 icon
       websiteDropdownImg.setAttribute("src", "assets/e621-icon.png");
       currentApi = API.E621;
       currentQuery.set("api", "E621");
@@ -50,6 +70,7 @@ fetch("header.html")
     // switch to derpi api when clicked
     selectorDerpibooru.onclick = function () {
       currentApi = API.DERPIBOORU;
+      // set dropdown image to derpi icon
       websiteDropdownImg.setAttribute("src", "assets/derpi-icon.png");
       currentQuery.set("api", "derpibooru");
       location.href =
@@ -84,17 +105,36 @@ if (window.location.toString().includes("settings.html")) {
   // we are on index/search page
   currentLocation = LOCATION.INDEX;
 
+  // this is the Packery grid
   var grid = document.querySelector('.grid');
   var pckry = new Packery(grid, {
     itemSelector: '.grid-item',
     gutter: '.gutter-sizer',
     columnWidth: '.grid-sizer',
-    percentPosition: true
+    percentPosition: true,
+    transitionDuration: '0.1s',
   });
 
+
+  // make the images larger if we have a mobile-sized window
+  if (isMobile()) {
+    resizeGrid("32%");
   pckry.layout();
+  }
+
+  // Add an event listener for the resize event
+  window.addEventListener('resize', function (event) {
+    if (isMobile()) {
+      resizeGrid("32%");
+    } else {
+      resizeGrid("18.4%");
+    }
+
+    pckry.layout();
+  });
 
   // TODO: check URL parameters for search and populate results
   // TODO: create post.ts object with fields for all relevant variables
   // use this for type checking: https://jvilk.com/MakeTypes/
 }
+
